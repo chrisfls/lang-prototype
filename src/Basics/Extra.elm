b9d6a85d194848ec627e1cf95c71905b58dc8aa3
Christian <2013206+kress95@users.noreply.github.com>
2022-05-27 02:55:15 -0300

    B

--- src/Basics/Extra.elm
+++ src/Basics/Extra.elm
@@ -1,16 +1,6 @@
 module Basics.Extra exposing (..)
 
 
-chain : a -> (a -> b) -> b
-chain =
-    (|>)
-
-
 flip : (a -> b -> c) -> b -> a -> c
 flip f a b =
     f b a
-
-
-uncurry : (a -> b -> c) -> ( a, b ) -> c
-uncurry f ( a, b ) =
-    f a b
