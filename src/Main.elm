aaf15cf43973d3fe9fba76510bb5a62b7b5b80b7
Christian <2013206+kress95@users.noreply.github.com>
2022-06-02 12:34:37 -0300

    Bump

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (..)
 
 import Bidir2 as Bidir exposing (Exp(..), Type(..))
 import Browser
+import Dict
 import Html exposing (text)
 
 
@@ -14,15 +15,14 @@ main =
         expr =
             Lam "f" (\f -> Lam "a" (\a -> Lam "b" (\b -> App (App f a) b)))
 
-        ann =
-            TArr (TArr (TVar False 0) (TArr (TVar False 1) (TVar False 2))) (TArr (TVar False 0) (TArr (TVar False 1) (TVar False 2)))
-
-        expr_ =
-            Ann ann expr
-
+        -- ann =
+        --     TArr (TArr (TVar False 0) (TArr (TVar False 1) (TVar False 2))) (TArr (TVar False 0) (TArr (TVar False 1) (TVar False 2)))
+        -- expr_ =
+        --     Ann ann expr
         _ =
-            Bidir.check expr_ Bidir.empty
-                |> Result.map (\( a, _ ) -> Debug.toString a)
+            Bidir.check expr Bidir.empty
+                |> Result.map
+                    (\( a, _ ) -> Bidir.toStringT a)
                 |> Debug.log "LANG2"
     in
     Browser.sandbox { init = (), update = always (always ()), view = always (text "") }
@@ -30,17 +30,14 @@ main =
 
 ssz : (b -> b) -> b -> b
 ssz s z =
-    -- behaves better
     s (s z)
 
 
 compose : (a -> b) -> (b -> c) -> a -> c
 compose =
-    -- behaves the same
     (>>)
 
 
 apply2 : (b -> c -> a) -> b -> c -> a
 apply2 f a b =
-    -- wip
     f a b
